b38b6468a0c6beb278184d9e0da547696547aa5a
ASHMIT-PROG <thatcodingguy1616@gmail.com>
2026-04-01 21:57:24 +0530

    count even numbers in a array

---
+++ ARRAYS/src/L1Q5__count_even_numbers.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class L1Q5__count_even_numbers {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        System.out.println(counteven(arr));
+    }
+
+    static int counteven(int[] num) {
+        if (num.length == 0) {
+            return -1;
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < num.length; i++) {
+            if (num[i] % 2 == 0) {
+                count++;// count = count + 1;
+                System.out.println("EVEN NUMBER"+num[i]+"NUMBER"+count);
+            }
+        }
+
+        return count;
+    }
+}
